917dfba9f5978f442caefe1dba4fe10022fec7da
majidfahad03-cell <majidfahad03@gmail.com>
2026-05-06 02:18:30 +0400

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,1 +1,26 @@
+import React from "react";
 
+const plants = [
+  { id: 1, name: "Aloe Vera", price: 25 },
+  { id: 2, name: "Snake Plant", price: 30 },
+  { id: 3, name: "Peace Lily", price: 35 },
+  { id: 4, name: "Monstera", price: 50 },
+];
+
+function ProductList() {
+  return (
+    <div>
+      <h2>Our Plants</h2>
+
+      {plants.map((plant) => (
+        <div key={plant.id}>
+          <h3>{plant.name}</h3>
+          <p>Price: AED {plant.price}</p>
+          <button>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
