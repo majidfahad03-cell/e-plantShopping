d7eba64da075dfee953ed5de77dd352d3dfe8a9a
majidfahad03-cell <majidfahad03@gmail.com>
2026-05-06 02:24:08 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(false);
+
   return (
     <div className="background-image">
-      <h1>Welcome to Paradise Nursery</h1>
-      <button>Get Started</button>
+      {!showProducts ? (
+        <>
+          <h1>Welcome to Paradise Nursery</h1>
+          <button onClick={() => setShowProducts(true)}>
+            Get Started
+          </button>
+        </>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
