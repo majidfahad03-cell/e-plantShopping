7c83c2ab4859f8df3c96321d51d62e166cfbccc8
majidfahad03-cell <majidfahad03@gmail.com>
2026-05-06 01:48:02 +0400

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>We provide the best plants for your home.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
