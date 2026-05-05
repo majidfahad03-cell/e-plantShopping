3672e6d2fd5ba95167981a853d6bfcab7b894e18
majidfahad03-cell <majidfahad03@gmail.com>
2026-05-06 02:25:34 +0400

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 function CartItem({ item, onRemove, onIncrease, onDecrease }) {
+  const total = item.price * item.quantity;
+
   return (
     <div>
       <h3>{item.name}</h3>
       <p>Price: AED {item.price}</p>
       <p>Quantity: {item.quantity}</p>
+      <p>Total: AED {total}</p>
 
       <button onClick={() => onIncrease(item.id)}>+</button>
       <button onClick={() => onDecrease(item.id)}>-</button>
