756402a9b27b1d7ba998b9b3a9a790a8e719fb11
majidfahad03-cell <majidfahad03@gmail.com>
2026-05-06 02:15:12 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+import "./App.css";
 
+function App() {
+  return (
+    <div className="background-image">
+      <h1>Welcome to Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
