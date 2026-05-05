d08614dd8f1a5bf017753d41bfc560fd6af22044
majidfahad03-cell <majidfahad03@gmail.com>
2026-05-06 02:20:11 +0400

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,17 @@
+import React from "react";
 
+function CartItem({ item, onRemove, onIncrease, onDecrease }) {
+  return (
+    <div>
+      <h3>{item.name}</h3>
+      <p>Price: AED {item.price}</p>
+      <p>Quantity: {item.quantity}</p>
+
+      <button onClick={() => onIncrease(item.id)}>+</button>
+      <button onClick={() => onDecrease(item.id)}>-</button>
+      <button onClick={() => onRemove(item.id)}>Remove</button>
+    </div>
+  );
+}
+
+export default CartItem;
